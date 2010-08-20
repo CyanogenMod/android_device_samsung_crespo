031a2615b30ed9fb132058d011e9ca52ac9d9a55
Jean-Baptiste Queru <jbq@google.com>
2010-08-19 19:59:07 -0700

    Improve crespo relationship with open-source
    -add a proper device.mk where open-source drivers can be added
    -add skeletons for extract-files.sh and unzip-files.sh
    -add a full_crespo config: the full open-source config on crespo
    
    Change-Id: I9de3afc372974225c3f892f776d3b418c3ba8213

--- device.mk
+++ device.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2008 The Android Open-Source Project
+# Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,18 +12,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-#
-# This file should set PRODUCT_MAKEFILES to a list of product makefiles
-# to expose to the build system.  LOCAL_DIR will already be set to
-# the directory containing this file.
-#
-# This file may not rely on the value of any variable other than
-# LOCAL_DIR; do not use any conditionals, and do not look up the
-# value of any variable that isn't set in this file or in a file that
-# it includes.
-#
-
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_crespo.mk \
-    $(LOCAL_DIR)/crespo.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2008 The Android Open-Source Project
+# Copyright (C) 2008 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,17 +14,10 @@
 # limitations under the License.
 #
 
+# This file is executed by build/envsetup.sh, and can use anything
+# defined in envsetup.sh.
 #
-# This file should set PRODUCT_MAKEFILES to a list of product makefiles
-# to expose to the build system.  LOCAL_DIR will already be set to
-# the directory containing this file.
-#
-# This file may not rely on the value of any variable other than
-# LOCAL_DIR; do not use any conditionals, and do not look up the
-# value of any variable that isn't set in this file or in a file that
-# it includes.
-#
+# In particular, you can add lunch options with the add_lunch_combo
+# function: add_lunch_combo generic-eng
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_crespo.mk \
-    $(LOCAL_DIR)/crespo.mk
+add_lunch_combo full_crespo-userdebug
