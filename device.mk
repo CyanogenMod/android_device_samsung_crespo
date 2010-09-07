fc6b9c33339ad505e418ed4e8e38503ca1e7bc0b
Jean-Baptiste Queru <jbq@google.com>
2010-09-07 10:48:21 -0700

    Revert "Get rid of system.prop"
    This reverts commit 5b0d6c4cc2745ce8c2ab727b914aa31d397d294a.

--- device.mk
+++ device.mk
@@ -84,11 +84,8 @@ PRODUCT_COPY_FILES += \
 # Note that the only such settings should be the ones that are too low-level to
 # be reachable from resources or other mechanisms.
 PRODUCT_PROPERTY_OVERRIDES := \
-	wifi.interface=eth0 \
-	dalvik.vm.heapsize=24m \
-	rild.libpath=/system/lib/libsec-ril.so \
-	rild.libargs=-d /dev/ttyS0 \
-	ro.sf.lcd_density=240
+       wifi.interface=eth0 \
+       dalvik.vm.heapsize=24m
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
