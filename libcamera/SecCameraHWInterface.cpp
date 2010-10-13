064455fe4cf93cf4944e0a8a454da3413891a53f
Jeong-Seok Yang <jseok.yang@samsung.com>
2010-10-13 16:59:50 +0900

    libcamera: Add missing copyright
    Change-Id: If70052d910cc904ac29f048ad28bcbb5fa0ddda0
    Signed-off-by: Jeong-Seok Yang <jseok.yang@samsung.com>

--- libcamera/SecCameraHWInterface.cpp
+++ libcamera/SecCameraHWInterface.cpp
@@ -1,7 +1,7 @@
 /*
 **
 ** Copyright 2008, The Android Open Source Project
-** Copyright@ Samsung Electronics Co. LTD
+** Copyright 2010, Samsung Electronics Co. LTD
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
