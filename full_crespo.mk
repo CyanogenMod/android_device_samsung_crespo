c0511fbd085e3aa3e0fcc81f88b28ce79bddb8b1
Jean-Baptiste Queru <jbq@google.com>
2010-10-22 14:54:28 -0700

    Use the new full_base - DO NOT MERGE
    Also a bit of cleanup
    
    Change-Id: I0f7bc2a8f28ac1ffa73ceb70d8f306ce0ac31ae3

--- full_crespo.mk
+++ full_crespo.mk
@@ -20,13 +20,13 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/crespo/device.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_crespo
 PRODUCT_DEVICE := crespo
+PRODUCT_BRAND := Android
 PRODUCT_MODEL := Full Android on Crespo
