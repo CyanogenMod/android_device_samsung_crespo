ce1b342c332b6d466a9cf78dafa82256e0270bb6
Jean-Baptiste Queru <jbq@google.com>
2011-03-31 09:08:46 -0700

    Don't build NFC in open-source builds. It's not meant to be used there.
    Bug: 4196003
    Change-Id: If27cdb243936dd6035ab9e6278a615e69dee9971

--- device_base.mk
+++ device_base.mk
@@ -96,13 +96,6 @@ PRODUCT_PACKAGES += \
 	libstagefrighthw \
 	com.android.future.usb.accessory
 
-# NFC
-PRODUCT_PACKAGES += \
-	libnfc \
-	libnfc_jni \
-	Nfc \
-	Tag
-
 # Input device calibration files
 PRODUCT_COPY_FILES += \
 	device/samsung/crespo/mxt224_ts_input.idc:system/usr/idc/mxt224_ts_input.idc
