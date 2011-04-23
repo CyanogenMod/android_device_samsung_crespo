f38c79d9a64cc9f15cdce969ae8cf308b534698a
Koushik Dutta <koushd@gmail.com>
2011-04-23 12:53:03 -0700

    recovery kernel
    Change-Id: I57b3a820fe1e5cb6657173456188cf5e5f07fa86

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,3 +83,5 @@ WIFI_DRIVER_MODULE_NAME     :=  "bcm4329"
 WIFI_DRIVER_MODULE_ARG      :=  "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/vendor/firmware/nvram_net.txt"
 
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/crespo/recovery_kernel
