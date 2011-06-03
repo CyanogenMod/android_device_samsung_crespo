bd99b4c555ac3c750947e51472a07de5c16fb734
Jean-Baptiste Queru <jbq@google.com>
2011-06-03 09:06:33 -0700

    Add NFC modules by default.
    They had to be disabled for a while because of a crashing issue
    that has since been resolved.
    
    Change-Id: Iab87c448e5552df0afe0d45fdfea1e06ced81bf4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -48,13 +48,6 @@ PRODUCT_PACKAGES := \\
 PRODUCT_COPY_FILES := \\
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
-# NFC packages
-PRODUCT_PACKAGES += \\
-    libnfc \\
-    libnfc_jni \\
-    Nfc \\
-    Tag
-
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/__MANUFACTURER__/__DEVICE__/overlay
 
