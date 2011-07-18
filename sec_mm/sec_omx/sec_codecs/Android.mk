5af084cebef71cbe8990a96e73b37cc5fdfce462
SeungBeom Kim <sbcrux.kim@samsung.com>
2011-07-18 11:18:27 +0900

    Add color space convertor in SEC_OMX
    NV12T to YUV420P
    NV12T to YUV420SP
    YUV420P to NV12T
    YUV420SP to NV12T
    
    Change-Id: I769bea28953786a9191824c488a633e8c997520f
    Signed-off-by: SeungBeom Kim <sbcrux.kim@samsung.com>

--- sec_mm/sec_omx/sec_codecs/Android.mk
+++ sec_mm/sec_omx/sec_codecs/Android.mk
@@ -4,4 +4,4 @@ include $(CLEAR_VARS)
 
 include   $(SEC_CODECS)/video/mfc_c110/dec/Android.mk
 include   $(SEC_CODECS)/video/mfc_c110/enc/Android.mk
-
+include   $(SEC_CODECS)/video/mfc_c110/csc/Android.mk
