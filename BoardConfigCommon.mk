83c630b1eeae5b6c9f3e93023859d6e2471b66c4
Dmitry Shmidt <dimitrysh@google.com>
2011-07-19 13:58:46 -0700

    wifi: Define proper path to use wifi_get_fw_path()
    Change-Id: Idcbecca34cc9811815e4fcfae480699afcd8241c
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,8 +71,8 @@ BOARD_WPA_SUPPLICANT_DRIVER := WEXT
 BOARD_WLAN_DEVICE := bcm4329
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
 WIFI_DRIVER_MODULE_PATH     := "/system/modules/bcm4329.ko"
-WIFI_DRIVER_FW_STA_PATH     := "/vendor/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_FW_AP_PATH      := "/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcm4329_apsta.bin"
 WIFI_DRIVER_MODULE_NAME     :=  "bcm4329"
 WIFI_DRIVER_MODULE_ARG      :=  "iface_name=wlan0 firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/vendor/firmware/nvram_net.txt"
 
