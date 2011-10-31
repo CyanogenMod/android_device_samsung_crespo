affccc0da1b1eab9d99837bab382d3c6a51b176f
Doug Zongker <dougz@android.com>
2011-10-31 15:12:04 -0700

    convert crespo recovery UI to C++ model
    Change-Id: I723984574080b94f990bccec8be221f4ab592970

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := eng
 LOCAL_C_INCLUDES += bootable/recovery
-LOCAL_SRC_FILES := recovery_ui.c
+LOCAL_SRC_FILES := recovery_ui.cpp
 
 # should match TARGET_RECOVERY_UI_LIB set in BoardConfig.mk
 LOCAL_MODULE := librecovery_ui_crespo
