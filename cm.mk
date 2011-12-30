7659938515fa237a4b65c0e1adc992e5c54a485c
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2011-12-29 21:46:43 -0500

    Override PRODUCT_* information
    Change-Id: I6f0cbe69ba9715fde8313b93ab5c09e8c1de9366

--- cm.mk
+++ cm.mk
@@ -10,4 +10,6 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=IML74K BUILD_FINGERPRINT=google/soju/crespo:4.0.3/IML74K/239410:user/release-keys PRIVATE_BUILD_DESC="soju-user 4.0.3 IML74K 239410 release-keys" BUILD_NUMBER=239410
 PRODUCT_NAME := cm_crespo
 PRODUCT_DEVICE := crespo
-
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus S
+PRODUCT_MANUFACTURER := Samsung
