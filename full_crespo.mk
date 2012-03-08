c9316f02e09f2aead4b6ca056c60c0764c00c63e
Chih-Chung Chang <chihchung@google.com>
2012-03-08 20:08:58 +0800

    Remove Camera package (it's merged into Gallery package).
    Change-Id: I4311a54f4dd8687f717735e8c45d2ffb982186c1

--- full_crespo.mk
+++ full_crespo.mk
@@ -22,10 +22,6 @@
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
-# Camera
-PRODUCT_PACKAGES := \
-    Camera
-
 # Live Wallpapers
 PRODUCT_PACKAGES += \
         LiveWallpapers \
