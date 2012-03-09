336c1bae9b4cc676d698169f6b873da232b9adfc
KalimochoAz <calimochoazucarado@gmail.com>
2012-03-09 17:44:26 +0100

    Update binary kernel
    - more RAM https://github.com/KalimochoAz/samsung-kernel-crespo/commit/538bae6ee1b5c15ba819b6e292c22d6c7349f5c4
    - added module to allow ntfs
    
    Change-Id: Ic1a37c65dc7e36222a04428b436b0296b3b152ef

---
+++ KernelModules.mk
@@ -0,0 +1,11 @@
+# Kernel Modules TO BE COPIED
+PRODUCT_COPY_FILES += \
+    device/samsung/crespo/bcm4329.ko:system/lib/modules/bcm4329.ko \
+    device/samsung/crespo/cifs.ko:system/lib/modules/cifs.ko \
+    device/samsung/crespo/exportfs.ko:system/lib/modules/exportfs.ko \
+    device/samsung/crespo/lockd.ko:system/lib/modules/lockd.ko \
+    device/samsung/crespo/nfsd.ko:system/lib/modules/nfsd.ko \
+    device/samsung/crespo/nfs.ko:system/lib/modules/nfs.ko \
+    device/samsung/crespo/ntfs.ko:system/lib/modules/ntfs.ko \
+    device/samsung/crespo/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
+    device/samsung/crespo/sunrpc.ko:system/lib/modules/sunrpc.ko
