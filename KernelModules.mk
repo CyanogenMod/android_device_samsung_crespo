906bb6f7d1dcc8581d81023c3f69041c871744f7
KalimochoAz <calimochoazucarado@gmail.com>
2012-03-11 22:13:08 +0100

    Rever the usage of kernel modules
    Since we don't have 04modules on init.d for cm9 this has not sense
    here
    
    Change-Id: I320dd85005a688eea26a1029f33720c093433bb0

--- KernelModules.mk
+++ KernelModules.mk
@@ -1,11 +1,4 @@
 # Kernel Modules TO BE COPIED
 PRODUCT_COPY_FILES += \
     device/samsung/crespo/bcm4329.ko:system/modules/bcm4329.ko \
-    device/samsung/crespo/cifs.ko:system/modules/cifs.ko \
-    device/samsung/crespo/exportfs.ko:system/modules/exportfs.ko \
-    device/samsung/crespo/lockd.ko:system/modules/lockd.ko \
-    device/samsung/crespo/nfsd.ko:system/modules/nfsd.ko \
-    device/samsung/crespo/nfs.ko:system/modules/nfs.ko \
-    device/samsung/crespo/ntfs.ko:system/modules/ntfs.ko \
-    device/samsung/crespo/scsi_wait_scan.ko:system/modules/scsi_wait_scan.ko \
-    device/samsung/crespo/sunrpc.ko:system/modules/sunrpc.ko
+    device/samsung/crespo/scsi_wait_scan.ko:system/modules/scsi_wait_scan.ko
