ccc62ab4f2f68fa55c18409e84ba1650551361bf
Colin Cross <ccross@android.com>
2012-03-26 16:25:50 -0700

    move hardware feature definitions
    Move the hardware feature xml files from frameworks/base/data/etc
    to frameworks/native/data/etc.
    
    Change-Id: I2ab863710024d1482756223cae712495029bbd6a

--- device.mk
+++ device.mk
@@ -36,7 +36,7 @@
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES := \
-	frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 # GPS config
 PRODUCT_COPY_FILES += \
