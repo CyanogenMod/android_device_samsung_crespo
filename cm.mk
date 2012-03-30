497bc0afa811906053d6c9344a0c8273f91f58b1
Bob Valantin <valantin89@gmail.com>
2012-03-30 22:58:09 +0200

    Update fingerprint to android 4.0.4 IMM76D
    Change-Id: I1d52a84848566989252edd97a76b311ae53fa436

--- cm.mk
+++ cm.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_FINGERPRINT=google/soju/crespo:4.0.3/IML74K/239410:user/release-keys PRIVATE_BUILD_DESC="soju-user 4.0.3 IML74K 239410 release-keys" BUILD_NUMBER=239410
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_FINGERPRINT=google/soju/crespo:4.0.4/IMM76D/299849:user/release-keys PRIVATE_BUILD_DESC="soju-user 4.0.4 IMM76D 299849 release-keys" BUILD_NUMBER=299849
 PRODUCT_NAME := cm_crespo
 PRODUCT_DEVICE := crespo
 PRODUCT_BRAND := Google
