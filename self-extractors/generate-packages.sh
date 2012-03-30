CyanogenMod/android_device_samsung_crespo4g

Self-extractors for IML74E
Change-Id: I0008320f039134aa004f07f0b1ba0f794316e574

self-extractors/generate-packages.sh
@@ -20,8 +20,9 @@
 # 236517 = IML70C
 # 237179 = IML73
 # 237867 = IML74B
-ZIP=sojus-ota-237867.zip
-BUILD=iml74b
+# 238432 = IML74E
+ZIP=sojus-ota-238432.zip
+BUILD=iml74e
 ROOTDEVICE=crespo
 DEVICE=crespo4g
 MANUFACTURER=samsung

CyanogenMod/android_device_samsung_crespo

Change-Id: Ibddac2800e0153af410a396358447a1baec60fbf

self-extractors/generate-packages.sh
@@ -31,8 +31,9 @@
 # 236517 = IML70C
 # 237179 = IML73
 # 237867 = IML74B
-ZIP=soju-ota-237867.zip
-BUILD=iml74b
+# 238432 = IML74E
+ZIP=soju-ota-238432.zip
+BUILD=iml74e
 ROOTDEVICE=crespo
 DEVICE=crespo
 MANUFACTURER=samsung