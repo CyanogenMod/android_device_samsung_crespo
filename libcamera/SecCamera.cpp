0ef29756cfe38ab0f62d3098afb3cb1573642100
Dima Zavin <dima@android.com>
2012-04-09 12:31:40 -0700

    libcamera: remove unused fimc_v4l2_s_ext_ctrl function
    Change-Id: I393a30310e2768931be7468f5f6f506d1658d655
    Signed-off-by: Dima Zavin <dima@android.com>

--- libcamera/SecCamera.cpp
+++ libcamera/SecCamera.cpp
@@ -469,26 +469,6 @@ static int fimc_v4l2_s_ctrl(int fp, unsigned int id, unsigned int value)
     return ctrl.value;
 }
 
-static int fimc_v4l2_s_ext_ctrl(int fp, unsigned int id, void *value)
-{
-    struct v4l2_ext_controls ctrls;
-    struct v4l2_ext_control ctrl;
-    int ret;
-
-    ctrl.id = id;
-    ctrl.reserved = value;
-
-    ctrls.ctrl_class = V4L2_CTRL_CLASS_CAMERA;
-    ctrls.count = 1;
-    ctrls.controls = &ctrl;
-
-    ret = ioctl(fp, VIDIOC_S_EXT_CTRLS, &ctrls);
-    if (ret < 0)
-        ALOGE("ERR(%s):VIDIOC_S_EXT_CTRLS failed\n", __func__);
-
-    return ret;
-}
-
 static int fimc_v4l2_g_parm(int fp, struct v4l2_streamparm *streamparm)
 {
     int ret;
