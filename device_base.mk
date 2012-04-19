9e322fc66b6aa27d0878c71abef600378bc2f826
KalimochoAz <calimochoazucarado@gmail.com>
2012-04-19 10:55:36 +0200

    Support inline kernel building
    Patch2: remove prebuild files
    
    Change-Id: I45e911eb29b74b049ff19db045e2f905421fc5df

--- device_base.mk
+++ device_base.mk
@@ -215,30 +215,4 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 # Screen size is "normal", density is "hdpi"
 PRODUCT_AAPT_CONFIG := normal hdpi
 
-# Kernel Modules to be copied
-ifeq ($(TARGET_PREBUILT_WIFI_MODULE),)
-LOCAL_WIFI_MODULE := device/samsung/crespo/bcm4329.ko
-else
-LOCAL_WIFI_MODULE := $(TARGET_PREBUILT_WIFI_MODULE)
-endif
-
-ifeq ($(TARGET_PREBUILT_SCSI_MODULE),)
-LOCAL_SCSI_MODULE := device/samsung/crespo/scsi_wait_scan.ko
-else
-LOCAL_SCSI_MODULE := $(TARGET_PREBUILT_SCSI_MODULE)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_WIFI_MODULE):system/modules/bcm4329.ko \
-    $(LOCAL_SCSI_MODULE):system/modules/scsi_wait_scan.ko
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/samsung/crespo/kernel
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
 $(call inherit-product-if-exists, vendor/nxp/pn544/nxp-pn544-fw-vendor.mk)
