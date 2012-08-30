9c771c99047a01a7cdcd42cfdad83e6fc6db906d
KalimochoAz <calimochoazucarado@gmail.com>
2012-08-30 20:59:23 +0200

    set default network GSM auto (PLR)
    Change-Id: I78951433fdccb31c22e8ac3ace93008a1a2c2896

--- device.mk
+++ device.mk
@@ -43,6 +43,9 @@ PRODUCT_COPY_FILES += \
         device/samsung/crespo/gps.xml:system/vendor/etc/gps.xml \
         device/samsung/crespo/gps.conf:system/etc/gps.conf
 
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.telephony.default_network=3
+
 $(call inherit-product, device/samsung/crespo/device_base.mk)
 
 # See comment at the top of this file. This is where the other
