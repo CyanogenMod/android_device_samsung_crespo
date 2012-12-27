2fb1dabe5394c4afd397177f3498abb1bb60830b
Geo Piskas <geopiskas@gmail.com>
2012-12-27 17:17:23 +0200

    CrespoParts: Added DisplayHomeAsUp feature.
    Enabled back feature by pressing the app icon at the top left corner.
    Screenshot: http://i.imgur.com/57u32.png
    
    Change-Id: Ie05f2b27e102da49f46c963a54d07b963aa3c34e

--- CrespoParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ CrespoParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -32,6 +32,7 @@ import android.os.IBinder;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.MenuItem;
 
 import com.cyanogenmod.settings.device.R;
 
@@ -63,6 +64,7 @@ public class DeviceSettings extends Activity {
         setContentView(mViewPager);
 
         final ActionBar bar = getActionBar();
+	bar.setDisplayHomeAsUpEnabled(true);
         bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE, ActionBar.DISPLAY_SHOW_TITLE);
         bar.setTitle(R.string.app_name);
@@ -84,6 +86,15 @@ public class DeviceSettings extends Activity {
         outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
     public static class TabsAdapter extends FragmentPagerAdapter
             implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
         private final Context mContext;
