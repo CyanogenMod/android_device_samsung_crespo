5abe3e95701afb8704adfe16d8e92faa9eac836b
winner00 <TOverturf@gmail.com>
2013-04-24 16:06:58 -0600

    Add PA overlay.

--- carbon.mk
+++ carbon.mk
@@ -20,6 +20,9 @@ $(call inherit-product, vendor/carbon/config/common_gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := NS
 
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_hdpi
+
 # languages
 PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
 # Inherit some common Carbon stuff.
