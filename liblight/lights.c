a0cb22cf011714d533006d693f5d467234ab4a6c
Joel Teichroeb <joel@teichroeb.net>
2013-08-22 12:19:35 -0700

    crespo: Add button backlight control to liblights
    Change-Id: I414fc809f92c2a72e28140dbbacbc06fac59c589

--- liblight/lights.c
+++ liblight/lights.c
@@ -32,6 +32,7 @@ static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
 char const *const LCD_FILE = "/sys/class/backlight/s5p_bl/brightness";
 char const *const LED_FILE = "/sys/class/misc/notification/led";
+char const *const LED_ENABLE_FILE = "/sys/class/misc/cypress_touchkey/backlight_status";
 
 static int write_int(char const *path, int value)
 {
@@ -96,6 +97,22 @@ static int set_light_backlight(struct light_device_t *dev,
 	return err;
 }
 
+static int set_light_buttons(struct light_device_t *dev,
+            struct light_state_t const *state)
+{
+    int touch_led_control = !!(state->color & 0x00ffffff);
+    int res;
+
+    ALOGD("set_light_buttons: color=%#010x, tlc=%u.", state->color,
+         touch_led_control);
+
+    pthread_mutex_lock(&g_lock);
+    res = write_int(LED_ENABLE_FILE, touch_led_control);
+    pthread_mutex_unlock(&g_lock);
+
+    return res;
+}
+
 static int close_lights(struct light_device_t *dev)
 {
 	ALOGV("close_light is called");
@@ -117,6 +134,8 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 		set_light = set_light_backlight;
 	else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
 		set_light = set_light_notifications;
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+        set_light = set_light_buttons;
 	else
 		return -EINVAL;
 
